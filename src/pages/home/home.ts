4021845cd94236121ad920ef1cbb8e8c5944850d
NinhLe <levanninh0101@gmail.com>
2017-03-06 00:10:38 +0700

    inital commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public base64Image: string;
+  constructor(public navCtrl: NavController) {
+
+  }
+  takePicture() {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+  selectPicture() {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+      targetHeight: 1000
+    }).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
